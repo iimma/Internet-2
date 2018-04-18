5651efd3646a4968c212bc755a16936a37fbe9f0
iimma <ros_imm@outlook.com>
2018-04-18 19:28:28 -0300

    ggwp

---
+++ JS/Scripts.js
@@ -0,0 +1,41 @@
+var users = [
+  {
+    name:"Mario"
+    lastname:"Dross"
+    age:67
+    city:"Atlantic City"
+    children:["amarillo","violeta","azul"]  
+  }, 
+  {
+    name:"Javier"
+    lastname:"Durazno"
+    age:7
+    city:"BS AS"
+    children:["kaka","jeje"]  
+  },  
+  {
+    name:"Facundo"
+    lastname:"Dross"
+    age:22
+    city:"Miami"
+    children:["cucumelo","javier","flor"]  
+  },  
+  {
+    name:"Pepe"
+    lastname:"Lehpu"
+    age:90
+    city:"Rosario"
+    children:["juan","pepito","animal"]  
+  },  
+  {
+    name:"Karlos"
+    lastname:"Oruga"
+    age:55
+    city:"Atlantic City"
+    children:["maaaaarta","jeenny","yeshikha"]  
+  },  
+]
+for(var i=0;i<users.lenght;i++){
+console.log(users[i] .name+ +users[i] .lastname + +"tiene"+ users[i] .children +"hijos")    
+}
+console.log();
