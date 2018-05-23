45dc926daa8ea2191cbfd6ae8257d13787e4a10f
iimma <ros_imm@outlook.com>
2018-05-23 19:27:36 -0300

    ggwp

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -1,58 +1,93 @@
+
+function sumar5(val){
+    return val+5;
+}
+sumar5();
+console.log(sumar5(20));
+
+function sumarL (valA,valB){
+    return valA+valB;
+}
+sumarL(100,5);
+
+function nombres (val1,val2){
+    return val1+", "+val2;
+}
+nombres("Valen","Amenabar");
+
 var users = [
-  {
-    name:"Mario",
-    lastname:"Dross",
-    age:67,
-    city:"Atlantic City",
-    children:["amarillo","violeta","azul"] , 
-  }, 
-  {
-    name:"Javier",
-    lastname:"Durazno",
-    age:7,
-    city:"BS AS",
-    children:["kaka","jeje"] , 
-  },  
-  {
-    name:"Facundo",
-    lastname:"Dross",
-    age:22,
-    city:"Miami",
-    children:["cucumelo","javier","flor"]  ,
-  },  
-  {
-    name:"Pepe",
-    lastname:"Lehpu",
-    age:90,
-    city:"Rosario",
-    children:["juan","pepito","animal"],  
-  },  
-  {
-    name:"Karlos",
-    lastname:"Oruga",
-    age:55,
-    city:"Atlantic City",
-    children:["maaaaarta","jeenny","yeshikha"],  
-  }  
+{name:"Valen",
+lastname:"Amenabar",
+age:21,
+city:"Rosario",
+children:["Juan"]},
+{name:"Ursula",
+lastname:"Soer",
+age:21,
+city:"Rosario",
+children:["Josefa","Pedro"]},
+{name:"Milagro",
+lastname:"Sala",
+age:21,
+city:"Rosario",
+children:["Pablo"]},
+{name:"Rei",
+lastname:"Ayanami",
+age:21,
+city:"Rosario",
+children:["Agustina"]},
+{name:"Johan",
+lastname:"Gomez",
+age:21,
+city:"Rosario",
+children:["Imanol","Facundo"]}
 ]
-for(var i=0;i<users.lenght;i++){
-console.log(users[i] .name+ +users[i] .lastname + +"tiene"+ users[i] .children +"hijos")    
-}
-console.log();
 
-function nombres (valA,valB){
-  return valA+", "+valB;
+
+var listUsers ={
+    listAll: function(){
+        for (var i=0; i<listUsers.users.length ; i++) {
+            $("#usersTable tbody").append(listUsers.assamble(listUsers.users[i]))
+        }
+    },
+    assamble: function(user){
+        return  "<tr>"
+        +"<td>"+user.Name+"</td>"
+        +"<td>"+user.email+"</td>"
+        +"<td>"+user.country+"</td>"
+        +"<td>"+user.profile+"</td>"
+        +"<td>"+user.age+"</td>"
+        +"<td>"+user.phone+"</td>"
+    +"</tr>"
+    },
+    listByAgeLimit: function (users, age) {
+        for (var i=0; i<users.length; i++) {
+            if (users[i].age<age)
+            console.log (listUsers.assamble (users[i]));
+        }
+    },
+    users: [],
+    setUsers: function (users) {
+        listUsers.users = users
+    }
 }
 
-function assamble(user){
-  return user.lastname+" "+user.name+", tiene "+user.age+", vive en "+user.city+", sus hijos son "+user.children
+function listUsersByAgeLimit (agelimit){
+    for (var i=0; i<users.length;i++){
+        if (users[i].age <= agelimit){
+        console.log(assamble(listUsers.users[i]));
+        }
+    }
+    }
+
+function toggieClass (){
+    if ($("h1").hasClass("active")){
+        $("h1").removeClass("active")
+        $("h1").addClass("disabled")
+    }
+else {
+    $("h1").removeClass("disabled")
+    $("h1").addClass("active")
+}
 }
 
-function listUsersByAgeLimit(ageLimit)  
-{
-for(var i=0;i<users.length;i++)
-  {
-  if(users[i].age<=ageLimit)
-  console.log(assamble(users[i]))
-  }
-}
--- JS/data.js
+++ JS/data.js
@@ -899,7 +899,6 @@ var usersItems = [
 		"salary": "$999.99",
 		"phone": "989-6883"
 	},
-	[
 	{
 		"Name": "Darius, lastname",
 		"email": "egestas.rhoncus.Proin@odio.ca",
